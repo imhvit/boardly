3b187e556d5f2b5a660b0f81442e3c5f5974140b
imhvit <yosifblancotafur2006@gmail.com>
2026-03-08 18:01:37 -0500

    Agregar ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import "../css/app.css";
+import { ZiggyVue } from "ziggy-js";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./pages/**/*.vue", { eager: true });
+        return pages[`./pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: "#ad46ff",
+    },
+});
