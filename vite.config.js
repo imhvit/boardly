784d375e9355ad897ac3dba7efce1f97e85b8490
imhvit <yosifblancotafur2006@gmail.com>
2026-03-08 18:01:12 -0500

    Configuraciones y dependencias

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
+        vue(),
     ],
     server: {
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+            "ziggy-js": path.resolve("vendor/tightenco/ziggy"),
         },
     },
 });
