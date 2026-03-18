8b03c70f65a4e4275027c1ec79200666fb99311d
imhvit <yosifblancotafur2006@gmail.com>
2026-03-18 14:25:52 -0500

    fix: correct route definition for boards and add card show route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\BoardController;
+use App\Http\Controllers\CardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,8 +16,10 @@ Route::prefix('app')->name('app.')->middleware('auth')->group(function () {
     // boards
     Route::get('boards', [AppController::class, 'boards'])->name('boards.index');
     Route::post('boards', [BoardController::class, 'store'])->name('boards.store');
+    Route::get('b/{public_id}/{slug?}', [BoardController::class, 'show'])->name('boards.show');
 
-    Route::get('b/{board}/{slug?}', [BoardController::class, 'show'])->name('boards.show');
+    // cards
+    Route::get('c/{public_id}/{slug?}', [CardController::class, 'show'])->name('cards.show');
     // templates
     Route::get('templates', [AppController::class, 'templates'])->name('templates.index');
 });
