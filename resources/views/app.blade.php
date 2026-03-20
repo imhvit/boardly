c62d3fbef3658a090bfda765309e7853bb9d23ed
imhvit <yosifblancotafur2006@gmail.com>
2026-03-19 21:52:39 -0500

    feat: preload font resources for improved loading performance

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preload" href="/fonts/geist/Geist[wght].woff2" crossorigin="anonymous" as="font" type="font/woff2">
+    <link rel="preload" href="/fonts/geist/Geist-Italic[wght].woff2" crossorigin="anonymous" as="font" type="font/woff2">
+    <link rel="preload" href="/fonts/geist/GeistMono[wght].woff2" crossorigin="anonymous" as="font" type="font/woff2">
+    <link rel="preload" href="/fonts/geist/GeistMono-Italic[wght].woff2" crossorigin="anonymous" as="font" type="font/woff2">
 
     @routes
     @vite('resources/js/app.js')
